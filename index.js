03549e759101c578ef15c4944b0e49712550b690
Jeric Frank Sasil <jericfrank@gmail.com>
2017-09-26 03:44:26 +0800

    vibrate

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ app.get( '/', ( req, res ) => {
  res.send( 'Were in Android world.' );
 });
 
+app.get( '/v', ( req, res ) => {
+ api.createCommand()
+    .vibrate()
+    .setDuration(1000)
+    .build()
+    .run();
+ res.send( 'The device must vibrate.' );
+});
+
 app.listen( 3000, function () {
  console.log( 'Running Android world...' );
 });
